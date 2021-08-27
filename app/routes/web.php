f3816366005312f1c8a83928a134716c05d6ae58
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-08-27 15:50:51 +0600

    Active Link Processs

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/dashboard', DashboardController::class);
+Route::get('admin/dashboard', DashboardController::class)->name('admin.dashboard');
