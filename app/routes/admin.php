65673dfa63a136421ae503d7ae955a39a2e45a9c
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-09-09 22:52:48 +0600

    Code Refactoring For Route

---
+++ app/routes/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Livewire\Admin\User\ListUsers;
+use App\Http\Livewire\Admin\Appoinments\ListAppoinments;
+use App\Http\Livewire\Admin\Appoinments\CreateAppoinmentForm;
+use App\Http\Livewire\Admin\Appoinments\UpdateAppoinmentForm;
+
+
+Route::get('dashboard', DashboardController::class)->name('dashboard');
+Route::get('user', ListUsers::class)->name('user');
+Route::get('appoinments', ListAppoinments::class)->name('appoinments');
+Route::get('appoinments/create', CreateAppoinmentForm::class)->name('appoinments.create');
+Route::get('appoinment/{PassAppoinment}/edit', UpdateAppoinmentForm::class)->name('appoinments.edit');
